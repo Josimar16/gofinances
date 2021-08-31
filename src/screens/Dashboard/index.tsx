da2be6782a6a74ff28e848dcf4f5e1a9427761ea
Josimar16 <josimarjr479@gmail.com>
2021-08-30 22:46:25 -0300

    feat: :sparkles: Criando header do projeto

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import theme from '../../global/styles/theme';
 
 import {
   Container,
-  Title
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
 } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Ola mundo!</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/49077388?s=400&u=ec9520ac11646eea256440b5db57ede4af4bf6be&v=4' }} />
+
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Josimar</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
